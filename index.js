cf9e4adba8aad815c6596ba0611216c15297726a
Rabia-Sohail <rabiasohail1209@gmail.com>
2024-04-15 10:47:20 +0500

    Created cli based lucky color game

---
+++ index.js
@@ -0,0 +1,12 @@
+import inquirer from "inquirer";
+let color = ["Red", "Blue", "White", "Black", "Brown", "Yellow", "Indigo", "Violet", "Orange", "Purple", "Golden", "Silver", "Grey", "Pink", "Green", "Sea Green", "Parrot Green", "Sky Blue", "Musturd", "Foan"];
+let name = await inquirer.prompt({
+    message: "Please enter your name",
+    type: "input",
+    name: "username",
+});
+let length1 = color.length;
+let inputLength = name.username.length;
+let rdmcolor = Math.floor(Math.random() * length1);
+let favcolor = color[rdmcolor];
+console.log(favcolor);
